e0a1cbd277428fa7226b1dcea691ea7ff2dd0aee
Yuki-Kaneda0808 <kaneda-y@biken.osaka-u.ac.jp>
2025-02-23 01:38:42 +0900

    Update content in App.js

---
+++ src/components/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="additional-info">
+      <h2>Affiliation</h2>
+      <p>Graduate School of Pharmaceutical Sciences, Osaka University (Concurrent)</p>
+      <p>Research Institute for Microbial Diseases</p>
+
+      <h2>Degree</h2>
+      <p>Master's (Pharmaceutical Sciences) (Mar, 2023, Osaka University)</p>
+      <p>Bachelor's (Pharmaceutical Sciences) (Mar, 2021, Osaka University)</p>
+
+      <h2>Research History</h2>
+      <p>Apr, 2023 - Present: Japan Society for the Promotion of Science</p>
+
+      <h2>Awards</h2>
+      <p>Oct, 2023: Excellent Performance Award (Extracurricular Activities Division), Ono Scholarship Foundation</p>
+      <p>Mar, 2023: 2023 Osaka University Sawai Memorial Pharmacy Friends Award for Young Researchers</p>
+
+      <h2>Research Areas</h2>
+      <p>Life Sciences / Laboratory Animal Science / Reproductive Engineering</p>
+      <p>Life Sciences / Developmental Biology / Fertilization</p>
+    </div>
+  );
+};
+
+export default About;
